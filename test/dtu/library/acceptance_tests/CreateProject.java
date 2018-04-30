6612856c8cfb6b173621a018e8e05745b861c2ab
michaelxuDTU <38072385+michaelxuDTU@users.noreply.github.com>
2018-04-30 16:20:02 +0200

    Update CreateProject.java

--- test/dtu/library/acceptance_tests/CreateProject.java
+++ test/dtu/library/acceptance_tests/CreateProject.java
@@ -1,82 +1,80 @@
-package dtu.library.acceptance_tests;
+package dtu.library.app;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import java.time.Year;
 import java.util.ArrayList;
-import cucumber.api.java.en.*;
-import dtu.library.app.*;
 
-public class CreateProject {
+public class Project {
 
-	private Project project;
-	private ArrayList <Worker> workers = new ArrayList<Worker>();
-	private String errmsg;
+	private String name,iD;
+	private Worker projectLeader;
+	private ArrayList <Activity> activities = new ArrayList <Activity>();
+	private ArrayList <Worker> workers = new ArrayList <Worker>();
 
-	public CreateProject() {
+	//Running number should be corrected
+	public Project(String name) {
+		this.name = name;
+		iD="" + Year.now().getValue() + "01";
 	}
 
-	@Given("^that a worker is logged in$")
-	public void thatAWorkerIsLoggedIn() throws Exception {
-		workers.add(new Worker("abcd"));
-	}
-
-	@When("^I create project with name \"([^\"]*)\"$")
-	public void iCreateProjectWithName(String name) throws Exception {
-		project = new Project(name);
-	}
-
-	@Then("^the projekt with name \"([^\"]*)\" and serial number \"([^\"]*)\" is created$")
-	public void theProjectWithNameAndSerialNumberIsCreated(String name, String iD) {
-		assertTrue(project.getName().equals(name) && project.getID().equals(iD));
-	}
-
-	@When("^I set worker with ID \"([^\"]*)\" as project leader$")
-	public void iSetWorkerWithIDAsProjectLeader(String iD) throws Exception {
-		Worker Brian = new Worker (iD);
-		project.setProjectLeader(Brian, Brian);
-	}
-
-	@Then("^worker with ID \"([^\"]*)\" is project leader on project \"([^\"]*)\"$")
-	public void workerWithIDIsProjectLeaderOnProject(String iD, String name) throws Exception {
-		assertTrue(project.getProjectLeader().getID().equals(iD));
-	}
-
-	@When("^worker \"([^\"]*)\" sets worker \"([^\"]*)\" as project leader$")
-	public void workerSetsWorkerAsProjectLeader(String iD1, String iD2) throws Exception {
-		try {
-			Worker Brian = new Worker(iD1);
-			Worker Rasmus = new Worker(iD2);
-			project.setProjectLeader(Brian, Rasmus);
-		} catch (Exception e) {
-			errmsg = e.getMessage();
+	public void setProjectLeader(Worker projectLeader, Worker developer) throws Exception{
+		if (this.projectLeader == null) {
+			this.projectLeader = developer;
+		} else if(this.projectLeader.getID().equals(projectLeader.getID())) {
+			this.projectLeader = developer;
+		} else {
+			throw new OperationNotAllowedException("Must be current project leader to change project leader");
 		}
 	}
 
-	@Then("^I get the project leader change error \"([^\"]*)\"$")
-	public void iGetTheProjectLeaderChangeError(String error) throws Exception {
-		assertTrue(errmsg.equals(error));
-	}
-
-	@When("^I add worker with ID \"([^\"]*)\" to project with name \"([^\"]*)\"$")
-	public void iAddWorkerWithIDToProjectWithName(String iD, String name) throws Exception {
-		try {
-			Worker Rasmus = new Worker(iD);
-			project.addWorker(Rasmus);
-		} catch (Exception e1) {
-			errmsg = e1.getMessage();
+	public void addActivity(Worker developer, Activity activity) throws Exception{
+		if (!(projectLeader == null) && developer.getID().equals(projectLeader.getID())) {
+			activities.add(activity);
+		}else {
+			throw new OperationNotAllowedException("Must be project leader to create new activity");
 		}
 	}
 
-	@Then("^worker \"([^\"]*)\" is working on project \"([^\"]*)\"$")
-	public void workerIsWorkingOnProject(String iD, String name) throws Exception {
-		assertTrue(project.getWorkers().get(0).getID().equals(iD));
+	public ArrayList <Activity> getActivities() {
+		return activities;
 	}
 
-	@Then("^I get the allready working on that error \"([^\"]*)\"$")
-	public void iGetTheAllreadyWorkingOnThatError(String error) throws Exception {
-		assertTrue(errmsg.equals(error));
+	public ArrayList <Worker> getWorkers() {
+		return workers;
 	}
 
+	public void addWorker(Worker worker) throws Exception {
+		for (int i = 0; i < workers.size(); i++) {
+			if (workers.get(i).getID().equals(worker.getID())) {
+				throw new OperationNotAllowedException("That worker is already working on this project");
+			}
+		}
+		workers.add(worker);
+	}
+	
+	public void removeWorker(Worker developer, String iD) throws Exception{
+		if(projectLeader.equals(developer) || developer.getID().equals(iD)) {
+			for (int i = 0; i < workers.size(); i++) {
+				if ( !(workers.get(i)==null) && workers.get(i).getID().equals(iD)) {
+					workers.remove(i);
+				} else {
+					throw new OperationNotAllowedException("No worker with ID \"" + iD + "\" exist");
+				}
+			}			
+		} else {
+			throw new OperationNotAllowedException("Must be either project leader or the worker to remove from project");
+		}
+	}
+
+	public Worker getProjectLeader() {
+		return projectLeader;
+	}
+
+	public String getID() {
+		return iD;
+	}
+
+	public String getName() {
+		return name;
+	}
 
 }
